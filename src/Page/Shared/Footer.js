be45bc5f0a26f4741873ad517de6a4b0fcc2562d
fahad09fahim <anik.fahim07@gmail.com>
2022-06-10 16:17:29 +0600

    social icon added

--- src/Page/Shared/Footer.js
+++ src/Page/Shared/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   const today = new Date();
@@ -7,6 +8,29 @@ const Footer = () => {
     <div>
       <footer className="footer footer-center p-4 text-white ">
         <div>
+          <span className="text-5xl flex flex-row gap-4">
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.facebook.com/fahad.fahim09"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://github.com/fahad09fahim"
+            >
+              <FaGithub />
+            </a>
+            <a
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.linkedin.com/in/md-fahad-al-fahim-2b2526197/"
+            >
+              <FaLinkedin />
+            </a>
+          </span>
           <p>Copyright © {year} - All right reserved by Md. Fahad Al Fahim </p>
         </div>
       </footer>
